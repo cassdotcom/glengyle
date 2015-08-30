f418edfb997d8bb10380a6069e0403ab31c137fc
cassdotcom <cassdotcom@users.noreply.github.com>
2015-08-30 22:28:08 +0100

    Update findlay.ps1

--- findlay.ps1
+++ findlay.ps1
@@ -5,6 +5,9 @@ $msgTable = Data {
         C_logPath = C:\\Users\\ac00418\\Documents\\glengyle\\data\\
         S_logPath = \\\\scotia.sgngroup.net\\dfs\\shared\\Syn4.2.3\\WOP 15-16\\ModelLogs\\
         S_logCreatedFile = \\\\scotia.sgngroup.net\\dfs\\shared\\Syn4.2.3\\TEST AREA\\ac00418\\OpsPlan\\scripts\\pshell\\logCreation.ps1
+        S_logDeleted = \\\\scotia.sgngroup.net\\dfs\\shared\\Syn4.2.3\\TEST AREA\\ac00418\\OpsPlan\\scripts\\pshell\\logDeleted.ps1
+        S_logRenamed = \\\\scotia.sgngroup.net\\dfs\\shared\\Syn4.2.3\\TEST AREA\\ac00418\\OpsPlan\\scripts\\pshell\\logRenamed.ps1
+        S_logChanged = \\\\scotia.sgngroup.net\\dfs\\shared\\Syn4.2.3\\TEST AREA\\ac00418\\OpsPlan\\scripts\\pshell\\logChanged.ps1
         startMsg = (Get-Date -UFormat "%Y-%m-%d %T")`tBegin script
         endMsg = (Get-Date -UFormat "%Y-%m-%d %T")`tFinish script
 '@
@@ -12,6 +15,10 @@ $msgTable = Data {
 
 
 function findlay {
+<#
+    .VERSION
+        1.0
+#>
 
     [CmdletBinding()]
     Param(
@@ -22,6 +29,8 @@ function findlay {
         [System.String]
         $thisFilt
     )
+    
+    # Version_0.2
 
     # path for logs
     if ( $thisPath ) {
@@ -49,7 +58,10 @@ function findlay {
             $filter = "*.csv"
 
             # path for created event scriptblock
-            $logCreatedFile = $msgTable.S_logCreated
+            $logCreatedFile = $msgTable.S_logCreatedFile
+            $logDeletedFile = $msgTable.S_logDeleted
+            $logChangedFile = $msgTable.S_logChanged
+            $logRenamedFile = $msgTable.S_logRenamed
 
         }
     }
@@ -66,6 +78,9 @@ function findlay {
 
     # created event scriptblock
     [ScriptBlock]$logCreated = Get-Command $logCreatedFile | select -ExpandProperty ScriptBlock
+    [ScriptBlock]$logDeleted = Get-Command $logDeletedFile | select -ExpandProperty ScriptBlock
+    [ScriptBlock]$logRenamed = Get-Command $logRenamedFile | select -ExpandProperty ScriptBlock
+    [ScriptBlock]$logChanged = Get-Command $logChangedFile | select -ExpandProperty ScriptBlock
 
 
 
@@ -73,14 +88,14 @@ function findlay {
     Register-ObjectEvent $findlay "Created" -Action $logCreated
 
     # action event
-    Register-ObjectEvent $findlay "Deleted" -Action $logCreated
+    Register-ObjectEvent $findlay "Deleted" -Action $logDeleted
 
     # action event
-    Register-ObjectEvent $findlay "Renamed" -Action $logCreated
+    Register-ObjectEvent $findlay "Renamed" -Action $logRenamed
 
     # action event
-    Register-ObjectEvent $findlay "Changed" -Action $logCreated
+    #Register-ObjectEvent $findlay "Changed" -Action $logChanged
+    
+    Show-Notification -msgText "Log Monitor is now running" -msgTitle "WOP 15/16" -alertLevel info
 
 }
-
-findlay
