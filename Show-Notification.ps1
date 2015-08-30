7e847651c0b4bfad131fc4ff283f6958d020fc55
cassdotcom <cassdotcom@users.noreply.github.com>
2015-08-30 22:17:43 +0100

    Update Show-Notification.ps1

--- Show-Notification.ps1
+++ Show-Notification.ps1
@@ -1,107 +1,37 @@
 function Show-Notification {
+
 # C:\Users\ac00418\Documents\WindowsPowerShell\FunctionLibrary\FunctionScripts\Show-Notification.ps1
-    [CmdletBinding()]
-    Param(
+
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
         [String]$msgText,
         [String]$msgTitle,
         [ValidateSet("info","warning","error")]
         [String]$alertLevel,
-        [Switch]$kill
+        [Int]$Timeout = 10000
     )
 
-    #Load the required assemblies
-    [void] [System.Reflection.Assembly]::LoadWithPartialName(“System.Windows.Forms”)
-    [void] [System.Reflection.Assembly]::loadwithpartialname("System.Drawing")
+    Add-Type -AssemblyName System.Windows.Forms
 
-    #Remove any registered events related to notifications
-    Remove-Event BalloonClicked_event -ea SilentlyContinue
-    Unregister-Event -SourceIdentifier BalloonClicked_event -ea silentlycontinue
-    Remove-Event BalloonClosed_event -ea SilentlyContinue
-    Unregister-Event -SourceIdentifier BalloonClosed_event -ea silentlycontinue
-    
-    #Create the notification object
-    $notification = New-Object System.Windows.Forms.NotifyIcon 
-    #Define the icon for the system tray
-    switch ($alertLevel) {
-        
-        'info' { $notification.Icon = [System.Drawing.SystemIcons]::Information }
-        'warning' { $notification.Icon = [System.Drawing.SystemIcons]::Warning }
-        'error' { $notification.Icon = [System.Drawing.SystemIcons]::Error }
-
+    if ($script:balloon -eq $null) {
+        $script:balloon = New-Object System.Windows.Forms.NotifyIcon
     }
 
-    #Display title of balloon window
-    $notification.BalloonTipTitle = $msgTitle
+    $balloon.Icon = "C:\Users\ac00418\Documents\glengyle\resources\icons\CHEVRON.ico"
+    $balloon.BalloonTipIcon = $alertLevel
+    $balloon.BalloonTipText = $msgText
+    $balloon.BalloonTipTitle = $msgTitle
+    $balloon.Visible = $true
 
-    #Type of balloon icon
-    $notification.BalloonTipIcon = $alertLevel
-
-    #Notification message
-    $notification.BalloonTipText = $msgText
-
-    #Make balloon tip visible when called
-    $notification.Visible = $True
-
-    ## Register a click event with action to take based on event
-    #Balloon message clicked
-    #register-objectevent $notification BalloonTipClicked BalloonClicked_event -Action {[System.Windows.Forms.MessageBox]::Show(“Balloon message clicked”,”Information”);$notification.Visible = $False;$notification.Dispose()} | Out-Null
-    register-objectevent $notification BalloonTipClicked BalloonClicked_event -Action {$notification.Visible = $False;$notification.Dispose()} | Out-Null
-
-    #Balloon message closed
-    register-objectevent $notification BalloonTipClosed BalloonClosed_event -Action {$notification.Visible = $False;$notification.Dispose()} | Out-Null
-
-    #Call the balloon notification
-    $notification.ShowBalloonTip(600)
-
-    #start-sleep (5)
-    #$notification.Visible = $false
-    #$notification.Dispose()
-
-
-
-
-
-
-    <#
-    # Load ballon form
-    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-
-    if ( $kill ) {
-        
-        # Kill balloon
-        $objNotifyIcon.Dispose()
-
-    } else { 
-    
-        #if ( $objNotifyIcon -eq $null ) {
-
-            $objNotifyIcon = $null
-            $objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon 
-
-        #}
-
-        Switch ( $alertLevel ) {
-
-            'error' { $barIcon = "red"; $msgIcon = "Error" }
-            'warning' { $barIcon = "blue"; $msgIcon = "Warning" }
-            'info' { $barIcon = "green"; $msgIcon = "Info" }
-
-        }
-
-        $notifyIcon = "C:\Users\ac00418\Documents\WindowsPowerShell\FunctionLibrary\FunctionData\icons\" + $barIcon + ".ico"
-
-        $objNotifyIcon.Icon = $notifyIcon
-        $objNotifyIcon.BalloonTipIcon = $msgIcon
-        $objNotifyIcon.BalloonTipText = $msgText
-        $objNotifyIcon.BalloonTipTitle = $msgTitle
-         
-        $objNotifyIcon.Visible = $True 
-        $objNotifyIcon.ShowBalloonTip(500)
-
-    }
-    #>
+    $balloon.ShowBalloonTip($Timeout)
 
 
 }
 
 
+function Kill-Balloon {
+
+    $Script:balloon.Dispose()
+    Remove-Variable -Scope Script -Name balloon
+
+}
