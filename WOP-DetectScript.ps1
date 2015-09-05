42ffc7b0e0e0d0c4753b54879ee9f60a8e5cc825
cassdotcom <cassdotcom@users.noreply.github.com>
2015-09-05 18:22:11 +0100

    Update WOP-DetectScript.ps1
    Rev 2

--- WOP-DetectScript.ps1
+++ WOP-DetectScript.ps1
@@ -2,7 +2,13 @@ $msgTable = Data {
 	ConvertFrom-StringData @'
 		outputDir=\\\\scotia.sgngroup.net\\dfs\\shared\\Syn4.2.3\\TEST AREA\\ac00418\\OpsPlan\\output\\
         LCSQuery=SELECT * FROM LCSDetail
-'@}
+		Run1=_R1.csv
+		Run2=_R2.csv
+		Run3=_R3.csv
+		StartScriptMsg=[$(Get-Date -UFormat "%Y-%m-%d %T")]`tBegin script
+		EndScript=[$(Get-Date -UFormat "%Y-%m-%d %T")]`tEnd script
+'@
+}
 <#
 .Synopsis
    Short description
@@ -30,28 +36,69 @@ $msgTable = Data {
 
 function WOP-DetectScripts {
 
+	[CmdletBinding()]
     Param(
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
         [System.Object]
-        $wopObj,
-        [System.String]
-        $wopYr
-    )
-
-    $missingScripts = @()
-
-    foreach ( $n in $wopObj ) {
-
-        $database = $n.FY1
-		$run1 = $msgTable.outputDir + $n.NUMBER + "_" + $wopYr + "_R1.csv"
-        $qry = $msgTable.LCSQuery
-
-        $scripts = Query-DataBase -database $database -query $qry
-
-        if ( $scripts.Count -ne 2 ) {
-
-            $missingScripts += $n
-            Write-Verbose "Missing`t$n.TITLE" }
-			
-	}
+        $wopObj
+    ) #end Param 
+	
+	Begin {
+	
+		Write-Verbose $msgTable.StartScriptMsg
+		$qry = $msgTable.LCSQuery 
+		Write-Verbose "[$(Get-Date -UFormat "%Y-%m-%d %T")]`tQuery: $($qry)"
+		$script:missingNets = @()
+		$script:foundNets = @()
 		
-}
+	} #end _BEGIN
+	
+	Process {
+	
+		# Database is model mdb		
+		# Pull db
+		Write-Verbose "[$(Get-Date -UFormat "%Y-%m-%d %T")]`t$($wopObj.TITLE)"
+        
+        # fy1
+        Write-Verbose "[$(Get-Date -UFormat "%Y-%m-%d %T")]`tYear: 1"
+        $wopYr = "FY1"
+        $scriptCount = 2
+		$scripts = Query-DataBase -database ($wopObj.FY1) -query $qry
+
+		if ( $scripts.Count -ne $scriptCount ) {
+
+			Write-Verbose "[$(Get-Date -UFormat "%Y-%m-%d %T")]`tMissing`t$n.NUMBER"
+			$script:missingNets += "$($wopObj.ID), 1"  }
+			
+		else { 
+
+			$script:foundNets += "$($wopObj.ID), 1"  }
+            
+        # fy5
+        Write-Verbose "[$(Get-Date -UFormat "%Y-%m-%d %T")]`tYear: 5"
+        $wopYr = "FY5"
+        $scriptCount = 3
+		$scripts = Query-DataBase -database ($wopObj.FY5) -query $qry
+
+		if ( $scripts.Count -ne $scriptCount ) {
+
+			Write-Verbose "[$(Get-Date -UFormat "%Y-%m-%d %T")]`tMissing`t$n.NUMBER"
+			$script:missingNets += "$($wopObj.ID), 5" }
+			
+		else { 
+
+			$script:foundNets += "$($wopObj.ID), 5"  }
+			
+	} #end Process
+	
+	End {
+	
+		$script:foundNets | Export-Clixml "S:\TEST AREA\ac00418\OpsPlan\data\FoundNets.xml" -NoClobber
+		$script:missingNets | Export-Clixml "S:\TEST AREA\ac00418\OpsPlan\data\MissingNets.cml" -NoClobber
+		
+		Write-Host "There were $(($script:foundNets).count) networks FOUND." -ForegroundColor DarkGreen
+		Write-Host "There were $(($script:missingNets).count) networks MISSING." -ForegroundColor DarkRed
+		
+	} #end END
+			
+} 
